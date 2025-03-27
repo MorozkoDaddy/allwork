f90b2c56c41862b114b4bdb195978489808dcf9f
MorozkoDaddy <sashaganus090@gmail.com>
2025-03-27 10:09:58 +0500

    Create 7.1.py

---
+++ 7.1.py
@@ -0,0 +1,19 @@
+import math
+
+class mathe:
+
+    def __init__(self, x):
+        self.x = x
+    
+    def raschet(self):
+        i = 0
+        a = 0
+        while i != 9:
+            b = int(input('Введите число'))
+            a = a + b  
+            i = i + 1 
+        return a
+               
+calculator = mathe(1)
+
+print(calculator.raschet())
