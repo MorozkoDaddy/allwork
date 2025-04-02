408d9e9b608512a88f1c260e88f70c3d9eb4b043
MorozkoDaddy <sashaganus090@gmail.com>
2025-04-02 11:12:44 +0500

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,15 @@
+x = int(input('Введите сторону : '))
+
+import math
+
+class calc:
+    def __init__(self, x):
+        self.x = x
+
+    def res(self):
+        y = 4*self.x
+        print(y)
+        
+calculator = Mathe(5)
+
+calculator.res()
