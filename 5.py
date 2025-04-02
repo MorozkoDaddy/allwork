9b2058515c4a189a06b8cb4aba9a3990f31e3784
MorozkoDaddy <sashaganus090@gmail.com>
2025-04-02 11:18:46 +0500

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,11 @@
+class Mathe:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def print(self):
+        print('',self.x,'\n', '\n', self.y)
+
+calculator = Mathe(1,2)
+
+calculator.print()
