27cebe7ea7c33fdf9b863fbbc593d8772efc7327
MorozkoDaddy <sashaganus090@gmail.com>
2025-04-02 11:16:59 +0500

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,19 @@
+class Mathe:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def subsctract(self):
+        self.x -= 1
+        self.y -= 1
+        self.z -= 1
+
+    def print(self):
+        print(self.x, '  ', self.y , '  ',self.z, '  ',)
+
+calculator = Mathe(48, 53, 151)
+
+calculator.subsctract()
+
+calculator.print()
